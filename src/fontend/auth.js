4f4477a2a5cae3d444b9381989fe58c4d41d51a3
Annguyenini <annini11234+github@gmail.com>
2025-09-17 22:40:25 -0700

    d

--- src/fontend/auth.js
+++ src/fontend/auth.js
@@ -8,28 +8,77 @@ const { width } = Dimensions.get('window');
 export  const AuthScreen= () => {
   const [showLogin, setShowLogin] = useState(false);
   const [showSignup, setShowSignup] = useState(false);
+  const [email,setEmail] = useState('');
   const [password,setPassWord] = useState ('');
+  const [confirmPassword,setConfirmPassWord] =useState('');
   const [username, setUserName] = useState ('');
+  const [displayName, setDisplayName] = useState('');
   const [alert, setAlertType] = useState('');
   const [showAlert, setShowAleart] = useState(false);
+  const [passwordMissingList, setPassWordMissingList] = useState([]);
+  const [showPasswordMissingList,setShowPML] = useState(false);
+  
   const submitRequest = ({action})=>{
     setShowAleart(false);
-    if(username.trim()===''||password.trim()===''){
-         Alert.alert('Error', 'Username and password cannot be empty!');
-         setAlertType("Username and password cannot be empty!");
+    if(action ==='Login'){
+      if(username.trim()===''||password.trim()===''){
+         Alert.alert('Error', 'Please Fill out all the requirement!');
+         setAlertType("Please Fill out all the requirement!");
         setShowAleart(true);
-
       return;
-  }
+    }
+    }
+    else if(action ==="Signup"){
+      setShowPML(false);
+      let res=[];
+      const specialCha = /[^A-Za-z0-9]/;
+      const emailCha = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if(username.trim()===''||password.trim()===''||displayName.trim()===''||confirmPassword===''||email===''){
+         Alert.alert('Error', 'Please Fill out all the requirement!');
+         setAlertType("Please Fill out all the requirement!");
+        setShowAleart(true);
+      return;
+      };
+      if(!emailCha.test(email)){
+        res.push('Invalid Email');
+      }
+      if(!/\d/.test(password)){
+        res.push('Missing Number!');
+      }
+      if(password.length<8){
+        res.push('Password have to be longer than 8!');
+      };
+      if(!specialCha.test(password)){
+        res.push("Missing special character!");
+      };
+      if(!/[A-Z]/.test(password)){
+        res.push('Missing upper letter!');
+      };
+      if(confirmPassword!=password){
+        res.push("Confirm password doesnt match!")
+      }
+      if(res.length>0){
+        setPassWordMissingList (res);
+        setShowPML(true);
+        return;
+      }
+    }
 }
   return (
     <>
       {/* Bottom buttons */}
       <View style={styles.buttonWrapper}>
-        <TouchableOpacity style={styles.button} onPress={() => setShowLogin(true)}>
+        <TouchableOpacity style={styles.button} onPress={() => {setShowLogin(true);
+          setPassWord('');
+          setUserName('');
+        }}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => setShowSignup(true)}>
+        <TouchableOpacity style={styles.button} onPress={() => {setShowSignup(true);
+          setPassWord('');
+          setUserName('');
+        }}>
           <Text style={styles.buttonText}>Signup</Text>
         </TouchableOpacity>
       </View>
@@ -39,7 +88,7 @@ export  const AuthScreen= () => {
         <OverlayCard title="LOGIN" onClose={() => setShowLogin(false)}>
           <TextInput style={authStyle.input} placeholder="UserName" value ={username} onChangeText={text=> setUserName(text)}/>
           <TextInput style={authStyle.input} placeholder="Password" value ={password} onChangeText={text=> setPassWord(text)}secureTextEntry />
-          <TouchableOpacity style={authStyle.submitButton} onPress ={()=>submitRequest('Login')}> 
+          <TouchableOpacity style={authStyle.submitButton} onPress ={()=>submitRequest({action:'Login'})}> 
              <Text style={authStyle.submitButtonText}>Submit</Text>
           </TouchableOpacity>
           {showAlert&&(<Text style={{textAlign:'center',marginTop: 10, color:'#FF0000'}}>{alert}</Text>)}
@@ -56,11 +105,26 @@ export  const AuthScreen= () => {
       {/* Signup Overlay */}
       {showSignup && (
         <OverlayCard title="SIGNUP" onClose={() => setShowSignup(false)}>
+          <TextInput style={authStyle.input} placeholder="Email" value ={email} onChangeText={text=> setEmail(text)}/>
+
+          <TextInput style={authStyle.input} placeholder="DisplayName" value ={displayName} onChangeText={text=> setDisplayName(text)}/>
+
           <TextInput style={authStyle.input} placeholder="UserName" value ={username} onChangeText={text=> setUserName(text)}/>
           <TextInput style={authStyle.input} placeholder="Password" value ={password} onChangeText={text=> setPassWord(text)}secureTextEntry />
-          <TouchableOpacity style={authStyle.submitButton} onPress ={()=>submitRequest('Signup')}> 
+          <TextInput style={authStyle.input} placeholder="Confirm Password" value ={confirmPassword} onChangeText={text=> setConfirmPassWord(text)}secureTextEntry />
+
+          <TouchableOpacity style={authStyle.submitButton} onPress ={()=>submitRequest({action:'Signup'})}> 
              <Text style={authStyle.submitButtonText}>Submit</Text>
           </TouchableOpacity>
+          {showPasswordMissingList&&(
+            <View>
+              {passwordMissingList.map((item, index)=>(
+                <Text key = {index} style={{ textAlign:'center',marginTop: 10, color:'#FF0000'}}>
+                  {item}
+                </Text>
+              ))}
+            </View>
+          )}
           {showAlert&&(<Text style={{textAlign:'center',marginTop: 10, color:'#FF0000'}}>{alert}</Text>)}
         </OverlayCard>
       )}
