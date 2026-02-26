18b0ea90afa1286beb680b50207cd2a409d1e727
An <annini11234+github@gmail.com>
2026-02-25 23:26:27 -0800

    testflight 3

--- src/config/config_api.js
+++ src/config/config_api.js
@@ -1,5 +1,5 @@
-// export const BASE_API='https://tripping.live'
-export const BASE_API='http://192.168.0.111:8000'
+export const BASE_API='https://tripping.live'
+// export const BASE_API='http://192.168.0.111:8000'
 
 export const LOGIN_API =BASE_API+'/auth/login';
 export const SIGN_UP_API =BASE_API+'/auth/signup';
