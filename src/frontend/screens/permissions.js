21d2d79376d5a477bbc21e66165892623f5a928e
An <annini11234+github@gmail.com>
2026-06-23 21:38:37 -0700

    test

--- src/frontend/screens/permissions.js
+++ src/frontend/screens/permissions.js
@@ -95,13 +95,12 @@ export default function PermissionsFlowScreen({ navigation }) {
           break;
         }
         case 1: {
-          const timeout = setTimeout(() => {
-            console.log("time out");
-          }, 5000);
           const { status } = await Location.requestBackgroundPermissionsAsync();
-          clearTimeout(timeout);
           if (status !== "granted") return setDenied(true);
-          setStep((s) => s + 1);
+          setTimeout(() => {
+            setStep((s) => s + 1);
+          }, 550);
+
           break;
         }
         case 2: {
